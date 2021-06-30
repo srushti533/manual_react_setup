904ba3a0b25076270bab0f3d9a355aa4aefbac39
srushti533 <srushtipandit533@gmail.com>
2021-06-30 14:34:54 +0530

    maualsetupreact

---
+++ .history/App_20210616203627.js
@@ -0,0 +1,4 @@
+//before es5
+function App() {}
+//after es5
+const App = () => {};
---
+++ App.js
@@ -0,0 +1,10 @@
+//before es5
+function App() {
+  return (
+    <div>
+      <p>this is from app component</p>
+    </div>
+  );
+}
+//after es5
+const App = () => {};
